7e285c6fd7c58304e6a9f4701ab1d08697047a80
jonas pauli <jonaspauli089@gmail.com>
2024-05-17 00:11:42 +0200

    update bash scripts

--- casper-contract-proto/compile.sh
+++ casper-contract-proto/compile.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rustup target add wasm32-unknown-unknown
 cargo build --release --target wasm32-unknown-unknown
 wasm-opt --strip-debug --signext-lowering ./target/wasm32-unknown-unknown/release/casper-contract-proto.wasm -o ../casper-contract-tests/binaries/casper-contract-proto-optimized.wasm
--- cspr-session/compile.sh
+++ cspr-session/compile.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rustup target add wasm32-unknown-unknown
 cargo build --release --target wasm32-unknown-unknown
 wasm-opt --strip-debug --signext-lowering ./target/wasm32-unknown-unknown/release/cspr-session.wasm -o ../casper-contract-tests/binaries/cspr-session-optimized.wasm
